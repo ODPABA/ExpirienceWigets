b390896b265c3b650b8a215bea99209138bef34d
Oddelenie dát - Magistrát Hlavného mesta SR Bratislavy <75433369+ODPABA@users.noreply.github.com>
2026-06-26 13:47:58 +0200

    Add files via upload

--- streetView/dist/runtime/translations/default.ts
+++ streetView/dist/runtime/translations/default.ts
@@ -2,5 +2,5 @@ export default {
   _widgetLabel: 'Street View',
   clickHint: 'Click on the map to place your point.',
   openInNewWindow: 'Open in new window',
-  noMapHint: 'Connect this widget to a map widget in the page layout.'
+  noMapHint: 'Select a map in the widget settings.'
 }
--- streetView/dist/runtime/translations/sk.ts
+++ streetView/dist/runtime/translations/sk.ts
@@ -2,5 +2,5 @@ export default {
   _widgetLabel: 'Street View',
   clickHint: 'Začnite kliknutím na mapu a umiestnite svoj bod.',
   openInNewWindow: 'Otvoriť v novom okne',
-  noMapHint: 'Prepojte tento widget s mapovým widgetom v rozložení stránky.'
+  noMapHint: 'V nastaveniach widgetu vyberte mapu.'
 }
